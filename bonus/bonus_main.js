d1faf2f5a37b2897f7617a54c6fc6483524184a2
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-06-05 14:38:56 +0200

    bonus completion

--- bonus/bonus_main.js
+++ bonus/bonus_main.js
@@ -7,9 +7,14 @@ lamp_toggle_El.addEventListener('click',
         if (lamp_off_El.classList == 'd-inline-block'){
             lamp_off_El.className = 'd-none'
             lamp_on_El.className = 'd-inline-block'
+            lamp_toggle_El.innerHTML = 'Spegni'
+            lamp_toggle_El.className = 'btn btn-secondary'
+            
         } else if(lamp_on_El.className == 'd-inline-block') {
             lamp_off_El.className = 'd-inline-block'
             lamp_on_El.className = 'd-none'
+            lamp_toggle_El.innerHTML = 'Accendi'
+            lamp_toggle_El.className = 'btn btn-warning'
         }
     }
 )
