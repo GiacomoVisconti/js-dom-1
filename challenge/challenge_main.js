8355e15ed62a2c4abd61f02c13fcc08bc3574efa
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-06-05 17:39:04 +0200

    challenge debug

--- challenge/challenge_main.js
+++ challenge/challenge_main.js
@@ -1,10 +1,28 @@
 const lamp = document.getElementById('lamp')
 const lamp_toggle_El = document.getElementById('lamp_toggle')
 
-lamp_toggle_El.addEventListener('click',function () {
-    lamp.src = './img/yellow_lamp.png'
-    lamp.alt = 'on lamp'
+console.log(document.querySelectorAll("#lamp_toggle span"));
+console.log(document.querySelectorAll("#lamp_toggle span")[0]);
 
+const btnTextOn = document.querySelectorAll("#lamp_toggle span")[0];
+const btnTextOff = document.querySelectorAll("#lamp_toggle span")[1];
+
+lamp_toggle_El.addEventListener('click',function () {
+
+    if (lamp.src.includes("white_lamp.png")){
+        // accende
+        lamp.src = './img/yellow_lamp.png'
+        lamp.alt = 'off lamp'
+
+    }else {
+
+        lamp.src = './img/white_lamp.png'
+        lamp.alt = 'on lamp'
+    }
+    
+    
+    btnTextOn.classList.toggle('d-none')
+    btnTextOff.classList.toggle('d-none')
 }
 )
 
