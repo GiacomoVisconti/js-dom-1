926508d570b5eca68ceb06e6fb43f7e73ed0ef10
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-06-05 16:17:09 +0200

    challenge completion

--- challenge/challenge_main.js
+++ challenge/challenge_main.js
@@ -1,21 +1,10 @@
-const lamp_on_El = document.getElementById('lamp_on')
-const lamp_off_El = document.getElementById('lamp_off')
+const lamp = document.getElementById('lamp')
 const lamp_toggle_El = document.getElementById('lamp_toggle')
 
-lamp_toggle_El.addEventListener('click',
-    function () {
-        if (lamp_off_El.classList == 'd-inline-block'){
-            lamp_off_El.className = 'd-none'
-            lamp_on_El.className = 'd-inline-block'
-            lamp_toggle_El.innerHTML = 'Spegni'
-            lamp_toggle_El.className = 'btn btn-secondary'
-            
-        } else if(lamp_on_El.className == 'd-inline-block') {
-            lamp_off_El.className = 'd-inline-block'
-            lamp_on_El.className = 'd-none'
-            lamp_toggle_El.innerHTML = 'Accendi'
-            lamp_toggle_El.className = 'btn btn-warning'
-        }
-    }
+lamp_toggle_El.addEventListener('click',function () {
+    lamp.src = './img/yellow_lamp.png'
+    lamp.alt = 'on lamp'
+
+}
 )
 
